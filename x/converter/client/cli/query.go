5242a5f0478f3d325dcfa042baae2da2559dd329
Dreamer <745124335@qq.com>
2023-05-18 16:47:40 +0800

    fix token-trace command error

--- x/converter/client/cli/query.go
+++ x/converter/client/cli/query.go
@@ -22,6 +22,7 @@ func GetQueryCmd() *cobra.Command {
 	cmd.AddCommand(
 		GetTokenPairsCmd(),
 		GetTokenPairCmd(),
+		GetTokenTraceCmd(),
 	)
 
 	return cmd
@@ -89,6 +90,37 @@ func GetTokenPairCmd() *cobra.Command {
 			}
 
 			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+	return cmd
+}
+
+// GetTokenTraceCmd queries a cross-chain token trace
+func GetTokenTraceCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "token-trace <class_id> <token_id>",
+		Short: "Get a cross-chain token trace",
+		Long:  "When the target chain uses the erc-721 port of nft-transfer to receive cross-chain nft, the system will deploy an erc-721 contract to map with the nft of the original chain. You can use this command to enter ibc_class_id and the nft_id of the original chain to query, and you will get a new nft generated across the chain",
+		Args:  cobra.ExactArgs(2),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			queryClient := types.NewQueryClient(clientCtx)
+			req := &types.QueryTokenTraceRequest{
+				ClassId: args[0],
+				TokenId: args[1],
+			}
+
+			data, err := queryClient.TokenTrace(cmd.Context(), req)
+			if err != nil {
+				return err
+			}
+			return clientCtx.PrintProto(data)
 		},
 	}
 
